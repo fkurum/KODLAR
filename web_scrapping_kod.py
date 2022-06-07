5c6c889ce247db1c44045d784325660824358954
fkurum <101329792+fkurum@users.noreply.github.com>
2022-06-07 18:55:17 +0300

    Update web_scrapping_kod.py

--- web_scrapping_kod.py
+++ web_scrapping_kod.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Zingat sitesinin yayınlamış olduğu demografik bilgi gibi bilgileri web scrapping metoduyla topladım. Verilerin çoğunluğu grafikler üzerinde bulunuyordu.
+# Grafiklere gelen veri de bir yerden gelmek zorunda olduğu için gelen tüm paketleri kayıt ederek veri içinden ayrıştırıldı.
 
 # In[ ]:
 
